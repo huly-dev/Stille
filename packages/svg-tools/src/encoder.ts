caff0351c57072c9dbd84d98631ff7e502348a17
Andrey Platov <andrey@hardcoreeng.com>
2024-03-24 15:34:34 +0100

    checkpoint
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- packages/svg-tools/src/encoder.ts
+++ packages/svg-tools/src/encoder.ts
@@ -5,7 +5,7 @@
 // © 2024 Hardcore Engineering Inc. All Rights Reserved.
 //
 
-import { Buffer } from 'node:buffer'
+import { SAXParser } from 'sax'
 
 export function encoder(maxBits: number, out: (value: number) => void) {
   let word: number = 0
@@ -17,7 +17,6 @@ export function encoder(maxBits: number, out: (value: number) => void) {
       if (bits < 0 || bits > maxBits) throw new Error('Invalid number of bits')
 
       for (let fit = maxBits - bit; bits > fit; fit = maxBits - bit) {
-        console.log('bit', bit, 'fit', fit, 'bits', bits)
         const toFit = value & ((1 << fit) - 1)
         word |= toFit << bit
         value >>>= fit
@@ -36,4 +35,77 @@ export function encoder(maxBits: number, out: (value: number) => void) {
       }
     },
   }
+}
+
+const TagBits = 1
+
+const Path = 0x0
+const Circle = 0x1
+
+const ClassBits = 3
+
+const NoClass = 0x00
+const countryID = ['UY', 'GE', 'RU', 'FR', 'US', 'MX', 'KZ']
+const countryName = ['Uruguay', 'Georgia', 'Russian Federation', 'France', 'United States', 'Mexico', 'Kazakhstan']
+
+export const encodeSVG = (svg: string) => {
+  let numbers = 0
+
+  const e = encoder(7, (_) => {
+    numbers++
+  })
+
+  const encodeAttributes = (attributes: Record<string, string>) => {
+    let clazz = NoClass
+
+    for (const [key, value] of Object.entries(attributes)) {
+      switch (key) {
+        case 'id':
+          clazz = countryID.findIndex((v) => v === value) + 1
+          break
+        case 'd':
+          parsePath(value, (command) => {
+            console.log(command)
+            e.writeUInt(command.command, CommandBits)
+          })
+          break
+        case 'class':
+          clazz = countryName.findIndex((v) => v === value) + 1
+          break
+        case 'name':
+          clazz = countryName.findIndex((v) => v === value) + 1
+          break
+        default:
+          console.log('Unhandled', key, value)
+      }
+    }
+
+    if (clazz !== NoClass) {
+      console.log('Classified', clazz)
+    }
+    e.writeUInt(clazz, ClassBits)
+  }
+
+  const parser = new SAXParser(true)
+  parser.onopentag = (node) => {
+    switch (node.name) {
+      case 'svg':
+        console.log('svg', node.attributes)
+        break
+      case 'path':
+        e.writeUInt(Path, TagBits)
+        encodeAttributes(node.attributes as Record<string, string>)
+        break
+      case 'circle':
+        e.writeUInt(Circle, TagBits)
+        break
+      default:
+        console.log('tag', node.name)
+    }
+  }
+  parser.write(svg).close()
+
+  console.log('encoded', numbers)
+
+  return {}
 }
--- packages/svg-tools/tests/encoder.test.ts
+++ packages/svg-tools/tests/encoder.test.ts
@@ -1,11 +1,17 @@
+//
+//   Huly® Platform™ Tools
+//   Licensed under the Eclipse Public License v2.0 (SPDX: EPL-2.0).
+//
+// © 2024 Hardcore Engineering Inc. All Rights Reserved.
+//
+
 import { expect, test } from 'bun:test'
 
-import { encoder } from '../src/encoder'
+import { encoder, encodeSVG } from '../src/encoder'
 
 test('encoder', () => {
   const result: number[] = []
   const e = encoder(8, (value) => {
-    console.log(value)
     result.push(value)
   })
   e.writeUInt(0b101, 3)
@@ -13,4 +19,9 @@ test('encoder', () => {
   e.writeUInt(0b10101, 5)
   e.flush()
   expect(result).toEqual([0b11101101, 0b1010])
+})
+
+test('svg-encoder', async () => {
+  const svg = await Bun.file(import.meta.dir + '/world.svg').text()
+  const result = encodeSVG(svg)
 })
