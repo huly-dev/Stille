e872bdeb9bc806c57ddba58c1cdd5a496ca0fed3
Andrey Platov <andrey@hardcoreeng.com>
2024-03-24 21:28:57 +0100

    checkpoint
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- packages/svgripper/src/analyze.ts
+++ packages/svgripper/src/analyze.ts
@@ -4,3 +4,13 @@
 //
 // © 2024 Hardcore Engineering Inc. All Rights Reserved.
 //
+
+import type { CID, Command } from "./svg";
+
+export const flatten = <Command>(commands: Command[]): Command[] => {
+  let lastCommand: CID | undefined
+
+  commands.forEach(command => {
+  if (command.command === l)
+  })
+}
